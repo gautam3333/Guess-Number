96e08131c407bd3345062ed94aead97e5ee15543
Gautam <gautam.sarkar3333@gmail.com>
2022-07-17 20:06:24 +0530

    Implementing highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,6 +13,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = ' #60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = secretNumber;
+    highScore = Math.max(score, highScore);
+    document.querySelector('.highscore').textContent = highScore;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -33,6 +36,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+// Again Button
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
